0a753fc10f87b8c45aad2cb61b8fdb08348e7d26
xingqiu17 <1065403018@qq.com>
2025-07-04 09:36:11 +0800

    第十九次提交-admin前端修复

--- quark-portal/src/main/java/com/quark/porent/config/WebMvcConfig.java
+++ quark-portal/src/main/java/com/quark/porent/config/WebMvcConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
-    @Value("${UPLOAD_PATH:D:/home/images/upload/}")
+    @Value("${UPLOAD_PATH:opt/images/upload/}")
     private String uploadPath;
 
 
--- quark-rest/src/main/java/com/quark/rest/utils/Constants.java
+++ quark-rest/src/main/java/com/quark/rest/utils/Constants.java
@@ -5,7 +5,7 @@ package com.quark.rest.utils;
  */
 public class Constants {
     //Upload常量
-    public static final String UPLOAD_PATH = "D:\\home\\images\\upload\\";
+    public static final String UPLOAD_PATH = "opt/images/upload/";
     public static final String STATIC_URL ="/images/upload/";
 
 }
